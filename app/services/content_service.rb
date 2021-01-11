f1d058e6878a99f3cd3e0b7202a0ec406a2ab64c
Sajad Rastegar <rastegar.sajjad@gmail.com>
2021-01-11 20:33:38 +0330

    Add content service

--- app/services/content_service.rb
+++ app/services/content_service.rb
@@ -1,8 +1,15 @@
+# Get any content through Contentful APIs based on the given content_type
 class ContentService
-  attr_reader :type
+  attr_reader :type, :client
 
-  def initialize(type: 'recipe')
+  def initialize(type:)
     @type = type
+    @client = Contentful::Client.new(
+      access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
+      space: ENV['CONTENTFUL_SPACE_ID'],
+      dynamic_entries: :auto,
+      raise_errors: true
+    )
   end
 
   # Returns all entries
@@ -13,17 +20,9 @@ class ContentService
   # Finds a specific entry
   def find(contentful_id)
     client.entries(:content_type => type, 'sys.id' => contentful_id).first
-  end
+  end 
 
-  private
-
-  def client
-    @client ||=
-      Contentful::Client.new(
-        access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
-        space: ENV['CONTENTFUL_SPACE_ID'],
-        dynamic_entries: :auto,
-        raise_errors: true
-      )
+  class << self
+    delegate :all, :find, to: :new
   end
 end
--- test/services/content_service_test.rb
+++ test/services/content_service_test.rb
@@ -4,15 +4,16 @@ class ContentServiceTest < ActiveSupport::TestCase
 
   setup do
     @content_service = ContentService.new(type: 'recipe')
-    @recipes = @content_service.all
+    @entries = @content_service.all
   end 
 
-  test "render_all should return an array" do
-    assert_kind_of Contentful::Array, @recipes
+  test "should get all entries" do
+    assert_kind_of Contentful::Array, @entries
   end
 
-  test "recipe should have correct format" do
-    recipe = @content_service.find(@recipes.first.id)
-    assert_kind_of Contentful::Entry, recipe
+  test "should find correct entry" do
+    entry_id = @entries.first.id
+    response_id = @content_service.find(entry_id).id
+    assert_equal entry_id, response_id
   end
 end
